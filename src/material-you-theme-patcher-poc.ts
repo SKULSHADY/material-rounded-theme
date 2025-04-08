3472caaaa52e40fba01f185701194b55f6eaa967
Nerwyn Singh <nerwyn1994@gmail.com>
2025-04-08 02:55:13 -0400

    fix horizontal overflow on hass-subpage

--- src/material-you-theme-patcher-poc.ts
+++ src/material-you-theme-patcher-poc.ts
@@ -16,7 +16,7 @@ import haSwitch from './css/ha-switch.css';
 import haTabs from './css/ha-tabs.css';
 import haToast from './css/ha-toast.css';
 import haUserBadge from './css/ha-user-badge.css';
-import hassTabsSubpage from './css/hass-tabs-subpage.css';
+import hassSubpage from './css/hass-subpage.css';
 import huiEntitiesCard from './css/hui-entities-card.css';
 import huiGridSection from './css/hui-grid-section.css';
 import huiRoot from './css/hui-root.css';
@@ -44,7 +44,8 @@ const elements: Record<string, string> = {
 	'paper-tabs': haTabs,
 	'ha-toast': haToast,
 	'ha-user-badge': haUserBadge,
-	'hass-tabs-subpage': hassTabsSubpage,
+	'hass-subpage': hassSubpage,
+	'hass-tabs-subpage': hassSubpage,
 	'hui-entities-card': huiEntitiesCard,
 	'hui-grid-section': huiGridSection,
 	'hui-root': huiRoot,
